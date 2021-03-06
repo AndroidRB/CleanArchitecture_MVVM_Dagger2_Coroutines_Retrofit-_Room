3e4d18ed4129913fa9bdba397d2b63c8977fa4bf
Radovan Bogdanic <radovan.bogdanic92@gmail.com>
2021-03-06 19:01:07 +0100

    Practice

--- app/src/main/java/com/practice/myapp/di/ApplicationGraph.kt
+++ app/src/main/java/com/practice/myapp/di/ApplicationGraph.kt
@@ -3,6 +3,7 @@ package com.practice.myapp.di
 import com.practice.data.di.DataModule
 import com.practice.domain.di.DomainModule
 import com.practice.presentation.di.FragmentSubComponent
+import com.practice.presentation.di.PresentationModule
 import com.practice.presentation.di.ViewModelModule
 import dagger.Component
 import dagger.android.support.AndroidSupportInjectionModule
@@ -11,14 +12,12 @@ import javax.inject.Singleton
 @Singleton
 @Component(
     modules = [
-        AndroidSupportInjectionModule::class,
-        ViewModelModule::class,
         AppModule::class,
+        PresentationModule::class,
         DataModule::class,
         DomainModule::class
     ]
 )
 interface ApplicationComponent {
-
     fun fragmentSubComponent(): FragmentSubComponent.Factory
 }
---
+++ presentation/src/main/java/com/practice/presentation/di/PresentationModule.kt
@@ -0,0 +1,7 @@
+package com.practice.presentation.di
+
+import dagger.Module
+
+@Module(includes = [ViewModelModule::class])
+class PresentationModule {
+}
